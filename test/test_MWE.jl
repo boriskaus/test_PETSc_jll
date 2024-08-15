3d51e2eff3b47c661cf8a7a85bb362e4874cb8ce
Boris Kaus <kaus@uni-mainz.de>
2024-08-15 21:27:09 +0200

    use version 3.21.

--- test/test_MWE.jl
+++ test/test_MWE.jl
@@ -5,8 +5,8 @@ using OpenBLAS32_jll, CompilerSupportLibraries_jll
 
 
 # ensure that we use the correct version of the package 
-#Pkg.add(url="https://github.com/boriskaus/PETSc_jll.jl")
-Pkg.add("PETSc_jll")
+Pkg.add(url="https://github.com/boriskaus/PETSc_jll.jl")
+#Pkg.add("PETSc_jll")
 using PETSc_jll
 
 #setup MPI
