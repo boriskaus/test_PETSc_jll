928e3d41d1d86fbb0b85c96f0181888a408dcd9b
Boris Kaus <kaus@uni-mainz.de>
2024-08-16 12:57:31 +0200

    use old runtests with registered Pkg

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,5 @@
 using Test, Pkg, Base.Sys
 
-# BLAS library used here:
-using OpenBLAS32_jll, CompilerSupportLibraries_jll
-            
-
 export mpirun, deactivate_multithreading, run_petsc_ex
 
 # ensure that we use the correct version of the package 
@@ -46,51 +42,6 @@ function deactivate_multithreading(cmd::Cmd)
     return cmd
 end
 
-# Shamelessly stolen from the tests of LBT 
-if Sys.iswindows()
-    LIBPATH_env = "PATH"
-    LIBPATH_default = ""
-    pathsep = ';'
-    binlib = "bin"
-    shlib_ext = "dll"
-elseif Sys.isapple()
-    LIBPATH_env = "DYLD_FALLBACK_LIBRARY_PATH"
-    LIBPATH_default = "~/lib:/usr/local/lib:/lib:/usr/lib"
-    pathsep = ':'
-    binlib = "lib"
-    shlib_ext = "dylib"
-else
-    LIBPATH_env = "LD_LIBRARY_PATH"
-    LIBPATH_default = ""
-    pathsep = ':'
-    binlib = "lib"
-    shlib_ext = "so"
-end
-
-function append_libpath(paths::Vector{<:AbstractString}, ENV::Vector{<:AbstractString})
-    
-    return join(vcat(paths..., ENV[first(findall(contains.(ENV,"$LIBPATH_env")))]), pathsep)
-end
-
-
-function add_LBT_flags(cmd::Cmd)
-    # using LBT requires to set the following environment variables
-    libdirs = unique(vcat(OpenBLAS32_jll.LIBPATH_list..., CompilerSupportLibraries_jll.LIBPATH_list...))
-    backing_libs = OpenBLAS32_jll.libopenblas_path
-    
-    env = Dict(
-        # We need to tell it how to find CSL at run-time
-        LIBPATH_env => append_libpath(libdirs, cmd.env),
-        "LBT_DEFAULT_LIBS" => backing_libs,
-        "LBT_STRICT" => 1,
-        "LBT_VERBOSE" => 1,
-    )
-    cmd = addenv(cmd, env)
-    
-    return cmd
-end
-
-
 """
     r = run_petsc_ex(ParamFile::String, cores::Int64=1, ex="ex4", args::String=""; wait=true, deactivate_multithreads=true, mpi_single_core=false)
 runs a petsc example
@@ -101,7 +52,6 @@ function run_petsc_ex(args::Cmd=``, cores::Int64=1, ex="ex4", ; wait=true, deact
         # Run LaMEM on a single core, which does not require a working MPI
         if ex=="ex4"
             cmd = `$(PETSc_jll.ex4()) $args`
-            #cmd = `$(PETSc_jll.ex4_int64_deb())  $args`
         elseif ex=="ex42"
             cmd = `$(PETSc_jll.ex42())  $args`
         elseif ex=="ex19"
@@ -113,21 +63,18 @@ function run_petsc_ex(args::Cmd=``, cores::Int64=1, ex="ex4", ; wait=true, deact
         if deactivate_multithreads
             cmd = deactivate_multithreading(cmd)
         end
-        
-      
-        cmd = add_LBT_flags(cmd)
-        
+
         r = run(cmd, wait=wait);
     else
         # create command-line object
         if ex=="ex4"
             cmd = `$(mpirun) -n $cores $(PETSc_jll.ex4_path) $args`
-            #cmd = `$(mpirun) -n $cores $(PETSc_jll.ex4_int64_deb_path) $args`
         elseif ex=="ex42"
             cmd = `$(mpirun) -n $cores $(PETSc_jll.ex42_path) $args`
         elseif ex=="ex19"
             cmd = `$(mpirun) -n $cores $(PETSc_jll.ex19_int64_deb_path) $args`
             #cmd = `$(mpirun) -n $cores $(PETSc_jll.ex19_path) $args`
+            
         else
             error("unknown example")
         end
@@ -135,9 +82,6 @@ function run_petsc_ex(args::Cmd=``, cores::Int64=1, ex="ex4", ; wait=true, deact
             cmd = deactivate_multithreading(cmd)
         end
 
-        # add stuff related to LBT
-        cmd = add_LBT_flags(cmd)
-        
         # Run example in parallel
         r = run(cmd, wait=wait);
     end
@@ -153,19 +97,12 @@ test_mumps = false
 if iswindows()
     is_parallel = false;        # activate parallel tests
     mpi_single_core = false;    # performs a single-core run without calling MPI
-    test_superlu_dist = false
-    test_mumps = false
-
 else
     is_parallel = true;         # activate parallel tests
     mpi_single_core = true;     # performs a single-core run without calling MPI
 end
-@testset verbose = true "ex19, ex42, ex4" begin
+@testset "ex19, ex42, ex4" begin
 
-    @testset "test_MWE" begin
-        include("test_MWE.jl")
-    end
-    
     if any(names(PETSc_jll) .== :ex19)
         # Note: ex19 is thew default test that PETSc performs @ the end of the installation process
         @testset "ex19 1: iterative" begin
@@ -214,7 +151,6 @@ end
         end
     end
 
-    
     @testset "ex42 1: serial" begin
         args = `-stokes_ksp_monitor_short -stokes_ksp_converged_reason -stokes_pc_type lu`;
         r = run_petsc_ex(args, 1, "ex42", mpi_single_core=mpi_single_core)
@@ -224,14 +160,6 @@ end
     @testset "ex42 2: mumps" begin
         if test_mumps & is_parallel
             args = `-stokes_ksp_monitor_short -stokes_ksp_converged_reason -stokes_pc_type lu -stokes_pc_factor_mat_solver_type mumps `;
-            r = run_petsc_ex(args, 2, "ex42")
-            @test r.exitcode == 0
-        end
-    end
-
-    @testset "ex42 2: superlu_dist" begin
-        if test_superlu_dist & is_parallel
-            args = `-stokes_ksp_monitor_short -stokes_ksp_converged_reason -stokes_pc_type lu -stokes_pc_factor_mat_solver_type superlu_dist `;
             r = run_petsc_ex(args, 2, "ex42")
             @test r.exitcode == 0
         end
@@ -317,6 +245,7 @@ end
         end
     end
 
+    
     @testset "ex4  4: direct superlu_dist" begin
         if test_superlu_dist & is_parallel
             args  = `-dim 2 -coefficients layers -nondimensional 0 -stag_grid_x 13 -stag_grid_y 8 -pc_type lu -pc_factor_mat_solver_type superlu_dist -ksp_converged_reason`;
@@ -406,9 +335,6 @@ end
             @test r.exitcode == 0
         end
     end
-    #=
-    =#
     
 
 end
-
