2c5acfc974c19a244d72e7c147c43c0a420354ea
Boris Kaus <kaus@uni-mainz.de>
2024-10-02 13:31:35 +0200

    using MPICH 4.1.2

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using OpenBLAS32_jll, CompilerSupportLibraries_jll
             
 export mpirun, deactivate_multithreading, run_petsc_ex
 
+
 # ensure that we use the correct version of the package 
 Pkg.add(url="https://github.com/boriskaus/PETSc_jll.jl")
 #Pkg.add("PETSc_jll")
